4de4d54620804d6c8b7d8bbd5d88ea6667e68189
David Raynes <rayners@gmail.com>
2016-10-05 17:15:08 -0400

    Updated to include /users/register test

--- models/user.js
+++ models/user.js
@@ -1,8 +1,16 @@
 'use strict';
+
+var bcrypt = require('bcrypt');
+
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
     username: DataTypes.STRING,
-    password: DataTypes.STRING,
+    password: {
+      type: DataTypes.STRING,
+      set: function(val) {
+        this.setDataValue('password', bcrypt.hashSync(val, 8));
+      }
+    },
     email: {
       type: DataTypes.STRING,
       validate: {
@@ -11,6 +19,11 @@ module.exports = function(sequelize, DataTypes) {
       }
     }
   }, {
+    instanceMethods: {
+      isValidPassword: function(password) {
+        return bcrypt.compareSync(password, this.password);
+      }
+    },
     classMethods: {
       associate: function(models) {
         // associations can be defined here
--- test/server/users-test.js
+++ test/server/users-test.js
@@ -1,60 +1,197 @@
 'use strict';
 
+// load the common test setup code
 require('../setup');
 
 // code to test
-var server = require('../../lib/server');
+var app = require('../../lib/server');
 
 // libraries
 var request = require('supertest-as-promised').agent,
-    User = require('../../models').User;
+  agent;
 
-describe('/users', function() {
-  var agent;
+var User = require('../../models').User;
 
+describe('users', function() {
   beforeEach(function() {
-    agent = request(server);
+    agent = request(app);
   });
 
   after(function() {
     return User.truncate();
   });
 
-  it('should have a /register page', function() {
-    return agent
-      .get('/users/register')
-      .expect(200);
-  });
-
-  it('should have a /login page', function() {
-    return agent
-      .get('/users/login')
-      .expect(200);
-  });
-
-  describe('when a user exists', function() {
-    var user;
-    beforeEach(function() {
-      return User.create({ username: 'MyFancyUsername',
-                          password: 'MyFancyPassword' })
-              .then(function(u) {
-                user = u;
-              });
+  describe('registration', function() {
+    it('should have a registration page', function() {
+      return agent
+        .get('/users/register')
+        .expect(200)
     });
 
-    it('should do …', function() {
-      console.log("User ID is " + user.id);
+    it('should log the new user in', function() {
       return agent
         .post('/users/register')
         .type('form')
         .send({
-          username: 'MyFancyUsername',
-          password: 'MyFancyPassword',
-          password_confirm: 'MyFancyPassword'
+          username: 'myNewUsername',
+          password: 'myFancyPassword',
+          password_confirm: 'myFancyPassword'
         })
-        .expect(200, /That username already exists\./);
+        .expect(302)
+        .expect('Location', '/')
+        .then(function() {
+          return agent
+            .get('/')
+            .expect(200, /Hello myNewUsername!/);
+          });
     });
 
+    it('should not allow registration if passwords do not match', function() {
+      return agent
+        .post('/users/register')
+        .type('form')
+        .send({
+          username: 'myNewUsername',
+          password: 'myFancyPassword',
+          password_confirm: 'myOtherFancyPassword'
+        })
+        .expect(200, /Passwords must match/);
+    });
+
+    describe('when a user already exists', function() {
+      var existingUser;
+      before(function() {
+        return User.create({ username: 'PreExistingUser1', password: 'TheirPassword' })
+          .then(function(user) {
+            existingUser = user;
+          });
+      });
+
+      it('should not allow registration of an existing username', function() {
+        return agent
+          .post('/users/register')
+          .type('form')
+          .send({
+            username: existingUser.username,
+            password: 'password',
+            password_confirm: 'password'
+          })
+          .expect(200, /User already exists/);
+      });
+
+      xit('should tell us the username is not avaialble', function() {
+        return agent
+          .post('/users/available')
+          .type('form')
+          .send({ username: existingUser.username })
+          .expect(200, { isAvailable: false });
+      });
+
+      xit('should tell us that another username is available', function() {
+        return agent
+          .post('/users/available')
+          .type('form')
+          .send({ username: existingUser.username + "new" })
+          .expect(200, { isAvailable: true });
+      });
+    });
   });
 
+  describe('login', function() {
+    it('should have a login page', function() {
+      return agent
+        .get('/users/login')
+        .expect(200);
+    });
+
+    it('should not login when there are no users', function() {
+      return agent
+        .post('/users/login')
+        .type('form')
+        .send({
+          username: 'someUsername',
+          password: 'somePassword'
+        })
+        .expect(200, /User not found/)
+        .then(function() {
+          return agent
+            .post('/users/login')
+            .set('Accept', 'application/json')
+            .send({
+              username: 'someUsername',
+              password: 'somePassword'
+            })
+            .expect(401, { error: 'User does not exist' });
+        });
+    });
+
+    describe('when there are users', function() {
+      before(function() {
+        return User.create({ username: 'PreExistingUser', password: 'TheirPassword' });
+      });
+
+      after(function() {
+        return User.truncate();
+      });
+
+      describe('for a form login', function() {
+        it('should login the user', function() {
+          return agent
+            .post('/users/login')
+            .type('form')
+            .send({
+              username: 'PreExistingUser',
+              password: 'TheirPassword'
+            })
+            .expect(302)
+            .expect('Location', '/')
+            .then(function() {
+              return agent
+                .get('/')
+                .expect(200, /Hello PreExistingUser!/);
+            });
+        });
+
+        it('should warn the user about incorrect passwords', function() {
+          return agent
+            .post('/users/login')
+            .type('form')
+            .send({
+              username: 'PreExistingUser',
+              password: 'myOtherFancyPassword'
+            })
+            .expect(200, /Password incorrect/);
+        });
+      });
+
+      describe('for an API login', function() {
+        it('should login the user', function() {
+          return agent
+            .post('/users/login')
+            .set('Accept', 'application/json')
+            .send({
+              username: 'PreExistingUser',
+              password: 'TheirPassword'
+            })
+            .expect(200, { success: true })
+            .then(function() {
+              return agent
+                .get('/')
+                .expect(200, /Hello PreExistingUser!/);
+            });
+        });
+
+        it('should warn the user about incorrect passwords', function() {
+          return agent
+            .post('/users/login')
+            .set('Accept', 'application/json')
+            .send({
+              username: 'PreExistingUser',
+              password: 'myOtherFancyPassword'
+            })
+            .expect(401, { error: 'Password incorrect' });
+        });
+      });
+    });
+  });
 });
