52a875e72b4fe67ac3e5aac7415a8cac3896f64a
David Raynes <rayners@gmail.com>
2017-06-19 20:22:32 -0400

    In class angular forms demo

--- app/fswd/registration.js
+++ app/fswd/registration.js
@@ -1,6 +1,27 @@
 
 var angular = require('angular');
 module.exports = angular.module('fswd.registration', [])
+    .directive('uniqueUsername', function($q, $http) {
+        return {
+                restrict: 'A',
+                require: '^ngModel',
+                link: function(scope, elem, attrs, ctrl) {
+                    ctrl.$asyncValidators.uniqueUsername = function(value) {
+                        return $q(function(resolve, reject) {
+                            $http.get('/users/available?username=' + value)
+                                .then(function(response) {
+                                    console.log(response.data);
+                                    if (response.data.isAvailable) {
+                                        resolve();
+                                    } else {
+                                        reject();
+                                    }
+                                });
+                        })
+                    };
+                }
+        };
+    })
     .controller('RegistrationController', function() {
         // this is the sample registration controller
         // eslint-disable-next-line
